efc36ef77ea16616bfbef96bd65a7c44eb93d28a
Martin <au648335@post.au.dk>
2022-11-08 15:29:10 +0100

    added baseline and functionality to analyse

--- Program/simple_algorithm.py
+++ Program/simple_algorithm.py
@@ -1,7 +1,23 @@
+from multiprocessing import Queue
 import Ialgorithm
+
+baseline_1 = 0.600
+baseline_2 = 0.600
 
 class SimpleAlgorithm(Ialgorithm):
 
-    def Analyse():
-        result = 0
-        return result
+    def Analyse(queue1, queue2):
+        
+        #upper muscel active
+        if (queue1.get()> baseline_1 and queue2.get()<baseline_2):
+            return 1
+
+        #lower muscel active
+        if (queue2.get()>baseline_2 and queue1.get()<baseline_1):
+            return 2
+
+    def Baseline():
+        #set baseline 1
+
+        #set baseline 2
+        None
