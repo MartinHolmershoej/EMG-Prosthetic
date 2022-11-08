9aa2b0c67fa47ed6af3331d5cd75ee3ec5cfbab1
Martin <au648335@post.au.dk>
2022-11-08 16:42:42 +0100

    same as before

--- Program/simple_algorithm.py
+++ Program/simple_algorithm.py
@@ -7,26 +7,28 @@ baseline_2 = 0.600 #default values
 class SimpleAlgorithm(Ialgorithm):
 
     def Analyse(queue1, queue2):
-        
-        #upper muscel active
-        if (queue1.get()> baseline_1 and queue2.get()<baseline_2):
-            return 1
+            
+            #upper muscel active
+            if (queue1.get()> baseline_1 and queue2.get()<baseline_2):
+                return 1
 
-        #lower muscel active
-        if (queue2.get()>baseline_2 and queue1.get()<baseline_1):
-            return 2
+            #lower muscel active
+            if (queue2.get()>baseline_2 and queue1.get()<baseline_1):
+                return 2
 
     def Baseline(queue1, queue2):
-        value1 = 0
-        value2 = 0
+            value1 = 0
+            value2 = 0
 
-        #set baseline 1 and 2
-        for x in range(10):
-            value1 += queue1.get()
-            value2 += queue2.get()
-        
-        baseline_1 = (value1/10.0)
-        baseline_2 = (value2/10.0)
+            #set baseline 1 and 2
+            for x in range(10):
+                value1 += queue1.get()
+                value2 += queue2.get()
+            
+            baseline_1 = (value1/10.0)
+            baseline_2 = (value2/10.0)
+
+            return baseline_1, baseline_2
 
 
         
