b5d372223384e67921670d53e7d61ff4bdb6e2a5
Martin <au648335@post.au.dk>
2022-11-22 13:11:17 +0100

    changed from led to motor

--- Program/motorController.py
+++ Program/motorController.py
@@ -1,4 +1,5 @@
 import gpiozero
+from Motor import Motor
 
 
 
@@ -18,10 +19,10 @@ import gpiozero
 
 
 # create 4 motor objects - no motors atm, so LED is created instead
-motor_1 = gpiozero.LED(12) #thumb
-motor_2 = gpiozero.LED(16) #index finger
-motor_3 = gpiozero.LED(20) #middle finger
-motor_4 = gpiozero.LED(21) #ring and little finger
+motor_1 = Motor(12) #thumb
+motor_2 = Motor(16) #index finger
+motor_3 = Motor(20) #middle finger
+motor_4 = Motor(21) #ring and little finger
 
 class MotorController():
     
@@ -29,44 +30,44 @@ class MotorController():
         
         match result:
             case 0:
-                motor_1.off()
-                motor_2.off()
-                motor_3.off()
-                motor_4.off()
+                motor_1.RunIn()
+                motor_2.RunIn()
+                motor_3.RunIn()
+                motor_4.RunIn()
                 
             case 1:
-                motor_1.on()
-                motor_2.on()
-                motor_3.on()
-                motor_4.on()  
+                motor_1.RunOut()
+                motor_2.RunOut()
+                motor_3.RunOut()
+                motor_4.RunOut()  
                   
             case 2: # difficut with leds
-                motor_1.off()
-                motor_2.off()
-                motor_3.off()
-                motor_4.off()
+                motor_1.RunIn()
+                motor_2.RunIn()
+                motor_3.RunIn()
+                motor_4.RunIn()
                 
             case 3:
-                motor_1.off()
-                motor_2.on()
-                motor_3.on()
-                motor_4.on()  
+                motor_1.RunIn()
+                motor_2.RunOut()
+                motor_3.RunOut()
+                motor_4.RunOut()  
                               
             case 4:
-                motor_1.on()
-                motor_2.on()
-                motor_3.off()
-                motor_4.on()   
+                motor_1.RunOut()
+                motor_2.RunOut()
+                motor_3.RunIn()
+                motor_4.RunOut()   
                              
             case 5:
-                motor_1.on()
-                motor_2.off()
-                motor_3.on()
-                motor_4.on()  
+                motor_1.RunOut()
+                motor_2.RunIn()
+                motor_3.RunOut()
+                motor_4.RunOut()  
                               
             case 6: # difficut with leds
-                motor_1.off()
-                motor_2.off()
-                motor_3.off()
-                motor_4.off()                
+                motor_1.RunIn()
+                motor_2.RunIn()
+                motor_3.RunIn()
+                motor_4.RunIn()                
         
