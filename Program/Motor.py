2063cdb0ef062b7bde909384975025fa535e2af1
Martin <au648335@post.au.dk>
2022-11-22 13:11:37 +0100

    Added motor class and its functions

--- Program/Motor.py
+++ Program/Motor.py
@@ -1,1 +1,18 @@
-#put the LEDs here
+import IMotor
+import gpiozero
+
+
+class Motor(IMotor):
+    
+    def __init__(self, pin):
+        self.pin = pin
+        
+    
+    def RunOut(self):
+        led = gpiozero.LED(self.pin)
+        led.on()
+        
+    
+    def RunIn():
+        led = gpiozero.LED(self.pin)
+        led.off()
