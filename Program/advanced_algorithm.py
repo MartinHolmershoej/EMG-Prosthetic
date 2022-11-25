66ae441edf95b3ea899f8825cb75e26d66e125a0
201904697 <60734588+201904697@users.noreply.github.com>
2022-11-25 11:44:34 +0100

    Advanced algorithm and testing

--- Program/advanced_algorithm.py
+++ Program/advanced_algorithm.py
@@ -1,7 +1,28 @@
+from multiprocessing import Queue
 import Ialgorithm
+
 
 class AdvancedAlgorithm(Ialgorithm.ABC):
 
     def Analyse(queueList):
-        result = 0
-        return result
+
+        queue1 = queueList[0]
+        queue2 = queueList[1]
+        queue3 = queueList[2]
+        queue4 = queueList[3]
+
+        eMGList = range(24)
+
+        for i in range(8):
+            queue1Value = queue1.get()
+            queue2Value = queue2.get()
+            queue3Value = queue3.get()
+            queue4Value = queue4.get()
+        
+            eMGList.append(queue1Value)
+            eMGList.append(queue2Value)
+            eMGList.append(queue3Value)
+            eMGList.append(queue4Value)
+            i += 1
+        return eMGList
+
