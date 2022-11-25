46f4c2da12a4de808e817cc97850cf64042d8aad
Martin <au648335@post.au.dk>
2022-11-25 13:20:28 +0100

    created a constructer

--- Program/motorController.py
+++ Program/motorController.py
@@ -17,55 +17,56 @@ from Motor import Motor
 # xxxxx = 8
 
 
-# create 4 motor objects - no motors atm, so LED is created instead
-motor_1 = Motor(12) #thumb
-motor_2 = Motor(16) #index finger
-motor_3 = Motor(20) #middle finger
-motor_4 = Motor(21) #ring and little finger
-
 class MotorController():
     
-    def MoveHand(result):
+    def __init__(self) -> None:
+        # create 4 motor objects - no motors atm, so LED is created instead
+        self.motor_1 = Motor(12) #thumb
+        self.motor_2 = Motor(16) #index finger
+        self.motor_3 = Motor(20) #middle finger
+        self.motor_4 = Motor(21) #ring and little finger
+    
+    def MoveHand(self, result):
         
         if result == 0:
-            motor_1.RunIn()
-            motor_2.RunIn()
-            motor_3.RunIn()
-            motor_4.RunIn()
+            self.motor_1.RunIn()
+            self.motor_2.RunIn()
+            self.motor_3.RunIn()
+            self.motor_4.RunIn()
                 
         elif result == 1:
-            motor_1.RunOut()
-            motor_2.RunOut()
-            motor_3.RunOut()
-            motor_4.RunOut()  
+            self.motor_1.RunOut()
+            self.motor_2.RunOut()
+            self.motor_3.RunOut()
+            self.motor_4.RunOut()  
                   
         elif result == 2:
-            motor_1.RunOut()
-            motor_2.RunIn()
-            motor_3.RunIn()
-            motor_4.RunOut()
+            self.motor_1.RunOut()
+            self.motor_2.RunIn()
+            self.motor_3.RunIn()
+            self.motor_4.RunOut()
                 
         elif result == 3: #difficult to make with leds
-            motor_1.RunIn()
-            motor_2.RunIn()
-            motor_3.RunOut()
-            motor_4.RunOut()  
+            self.motor_1.RunIn()
+            self.motor_2.RunIn()
+            self.motor_3.RunOut()
+            self.motor_4.RunOut()  
                               
         elif result == 4:
-            motor_1.RunOut()
-            motor_2.RunOut()
-            motor_3.RunIn()
-            motor_4.RunOut()   
+            self.motor_1.RunOut()
+            self.motor_2.RunOut()
+            self.motor_3.RunIn()
+            self.motor_4.RunOut()   
                              
         elif result == 5:
-            motor_1.RunOut()
-            motor_2.RunIn()
-            motor_3.RunOut()
-            motor_4.RunOut()  
+            self.motor_1.RunOut()
+            self.motor_2.RunIn()
+            self.motor_3.RunOut()
+            self.motor_4.RunOut()  
                               
         elif result == 6: #difficult to make with leds
-            motor_1.RunOut()
-            motor_2.RunOut()
-            motor_3.RunIn()
-            motor_4.RunIn()                
+            self.motor_1.RunOut()
+            self.motor_2.RunOut()
+            self.motor_3.RunIn()
+            self.motor_4.RunIn()                
         
