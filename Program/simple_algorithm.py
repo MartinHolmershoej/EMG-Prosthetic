2518cce9438f594948c656a99dc45918bf855eef
Martin <au648335@post.au.dk>
2022-11-25 17:24:40 +0100

    added the motorControl function call to simplealgo

--- Program/simple_algorithm.py
+++ Program/simple_algorithm.py
@@ -1,4 +1,4 @@
-from multiprocessing import Queue
+from motorController import MotorController
 import Ialgorithm
 
 
@@ -8,8 +8,9 @@ class SimpleAlgorithm(Ialgorithm.ABC):
     def __init__(self) -> None:
         self.baseline_1 = 0.600
         self.baseline_2 = 0.600
-
-
+        self.result = 0
+        self.motor = MotorController()
+        
 
     def Analyse(self, queueList, gripGroup):
         queue1 = queueList[0]
@@ -20,27 +21,30 @@ class SimpleAlgorithm(Ialgorithm.ABC):
         #upper muscel active
         if (queue1Value > self.baseline_1 and queue2Value < self.baseline_2):
             if gripGroup == 1:
-                return 1
+                self.result = 1
 
             elif gripGroup == 2:
-                return 3
+                self.result = 3
             
             elif gripGroup == 3:
-                return 5
+                self.result = 5
 
         #lower muscel active
         elif (queue2Value > self.baseline_2 and queue1Value < self.baseline_1):
             if gripGroup == 1:
-                return 2
+                self.result = 2
 
             elif gripGroup == 2:
-                return 4
+                self.result = 4
             
             elif gripGroup == 3:
-                return 6
+                self.result = 6
             
         else:
-            return 0
+            self.result = 0
+        
+        #Call the motorcontroller her
+        self.motor.MoveHand(self.result)
 
     def Baseline(self, queueList):
         value1 = 0
@@ -49,12 +53,15 @@ class SimpleAlgorithm(Ialgorithm.ABC):
         queue2 = queueList[1]
         
         # #set baseline 1 and 2
-        for x in range(10):
+        for x in range(20):
              value1 += queue1.get()
              value2 += queue2.get()
             
-        self.baseline_1 = round(value1/10.0,2)
-        self.baseline_2 = round(value2/10.0,2)
+        self.baseline_1 = (round(value1/10.0,2))
+        self.baseline_2 = (round(value2/10.0,2))
+        
+        print(str(self.baseline_1))
+        print(str(self.baseline_2))
 
     
         
