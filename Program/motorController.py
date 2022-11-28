a516c218af0dda7ff6ab158a3e006c81433ac9be
Martin <au648335@post.au.dk>
2022-11-28 11:00:35 +0100

    fixed a bug, that made the leds flash

--- Program/motorController.py
+++ Program/motorController.py
@@ -25,48 +25,56 @@ class MotorController():
         self.motor_2 = Motor(16) #index finger
         self.motor_3 = Motor(20) #middle finger
         self.motor_4 = Motor(21) #ring and little finger
+        self.currentResult = 0
     
     def MoveHand(self, result):
         
-        if result == 0:
+        if result == 0 and not self.currentResult == 0:
             self.motor_1.RunIn()
             self.motor_2.RunIn()
             self.motor_3.RunIn()
             self.motor_4.RunIn()
+            self.currentResult = 0
                 
-        elif result == 1:
+        elif result == 1 and not self.currentResult == 1:
             self.motor_1.RunOut()
             self.motor_2.RunOut()
             self.motor_3.RunOut()
-            self.motor_4.RunOut()  
+            self.motor_4.RunOut()
+            self.currentResult = 1  
                   
-        elif result == 2:
+        elif result == 2 and not self.currentResult == 2:
             self.motor_1.RunOut()
             self.motor_2.RunIn()
             self.motor_3.RunIn()
             self.motor_4.RunOut()
+            self.currentResult = 2
                 
-        elif result == 3: #difficult to make with leds
+        elif result == 3 and not self.currentResult == 3: #difficult to make with leds
             self.motor_1.RunIn()
             self.motor_2.RunIn()
             self.motor_3.RunOut()
-            self.motor_4.RunOut()  
+            self.motor_4.RunOut()
+            self.currentResult = 3  
                               
-        elif result == 4:
+        elif result == 4 and not self.currentResult == 4:
             self.motor_1.RunOut()
             self.motor_2.RunOut()
             self.motor_3.RunIn()
-            self.motor_4.RunOut()   
+            self.motor_4.RunOut()
+            self.currentResult = 4   
                              
-        elif result == 5:
+        elif result == 5 and not self.currentResult == 5:
             self.motor_1.RunOut()
             self.motor_2.RunIn()
             self.motor_3.RunOut()
-            self.motor_4.RunOut()  
+            self.motor_4.RunOut()
+            self.currentResult = 5  
                               
-        elif result == 6: #difficult to make with leds
+        elif result == 6 and not self.currentResult == 6: #difficult to make with leds
             self.motor_1.RunOut()
             self.motor_2.RunOut()
             self.motor_3.RunIn()
-            self.motor_4.RunIn()                
+            self.motor_4.RunIn()
+            self.currentResult = 6                
         
