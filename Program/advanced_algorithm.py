8149ffbea76640f8cd323ffc140a1b8bde8a04cb
Martin <au648335@post.au.dk>
2022-12-06 14:26:10 +0100

    Fixed a list mistake, and changed a method name

--- Program/advanced_algorithm.py
+++ Program/advanced_algorithm.py
@@ -10,14 +10,14 @@ class AdvancedAlgorithm(Ialgorithm.ABC):
         self.eMGList = []
         self.prediction = 0
 
-    def AddToEMGList(self, queueList, eMGList):
+    def AddToEMGList(self, queueList):
 
         queue1 = queueList[0]
         queue2 = queueList[1]
         queue3 = queueList[2]
         queue4 = queueList[3]
 
-        if len(eMGList) == 0:
+        if len(self.eMGList) == 0:
             for i in range(8):
                 queue1Value = queue1.get()
                 queue2Value = queue2.get()
@@ -25,27 +25,27 @@ class AdvancedAlgorithm(Ialgorithm.ABC):
                 queue4Value = queue4.get()
             
 
-                eMGList.append(queue1Value)
-                eMGList.append(queue2Value)
-                eMGList.append(queue3Value)
-                eMGList.append(queue4Value)
+                self.eMGList.append(queue1Value)
+                self.eMGList.append(queue2Value)
+                self.eMGList.append(queue3Value)
+                self.eMGList.append(queue4Value)
         
-        elif len(eMGList) == 32:
+        elif len(self.eMGList) == 32:
             queue1Value = queue1.get()
             queue2Value = queue2.get()
             queue3Value = queue3.get()
             queue4Value = queue4.get()
 
-            eMGList.append(queue1Value)
-            eMGList.append(queue2Value)
-            eMGList.append(queue3Value)
-            eMGList.append(queue4Value)
+            self.eMGList.append(queue1Value)
+            self.eMGList.append(queue2Value)
+            self.eMGList.append(queue3Value)
+            self.eMGList.append(queue4Value)
 
             for i in range(4):
-                eMGList.pop(0)
+                self.eMGList.pop(0)
 
         else:
-            eMGList.clear()
+            self.eMGList.clear()
 
             for i in range(8):
                 queue1Value = queue1.get()
@@ -53,21 +53,13 @@ class AdvancedAlgorithm(Ialgorithm.ABC):
                 queue3Value = queue3.get()
                 queue4Value = queue4.get()
             
-                eMGList.append(queue1Value)
-                eMGList.append(queue2Value)
-                eMGList.append(queue3Value)
-                eMGList.append(queue4Value)
+                self.eMGList.append(queue1Value)
+                self.eMGList.append(queue2Value)
+                self.eMGList.append(queue3Value)
+                self.eMGList.append(queue4Value)
                 
-        self.eMGList = eMGList
-        return self.eMGList
-
-    def predict(self, eMGList):
+    def Analyse(self,):
         clf = load(r"C:\Users\Vinni\OneDrive\Dokumenter\GitHub\EMG-Prosthetic\Program\RF_Minimalized.joblib")
-        self.prediction = clf.predict(np.asarray(eMGList).reshape(1,-1))
+        self.prediction = clf.predict(np.asarray(self.eMGList).reshape(1,-1))
 
         return self.prediction
-
-# Skal køres i denne rækkefølge:
-# test = AdvancedAlgorithm()
-# test.AddToEMGList(test.eMGList)
-# test.predict(test.eMGList)
